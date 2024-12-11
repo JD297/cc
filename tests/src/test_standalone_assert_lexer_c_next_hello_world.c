37aab5a8c6f1e11cbd1fb429ea1a0d498751a748
JD297 <jan.dommasch297@gmail.com>
2024-12-11 22:40:26 +0100

    Refactored expected test values to header file

--- tests/src/test_standalone_assert_lexer_c_next_hello_world.c
+++ tests/src/test_standalone_assert_lexer_c_next_hello_world.c
@@ -8,25 +8,11 @@
 
 int main()
 {
+	#include "asset_test_hello_world.h"
+
 	Lexer_C *lexer;
 
 	assert((lexer = lexer_c_create_lexer("tests/assets/asset_test_hello_world.c")) != LEXER_CREATION_FAILED);
-
-	#define ASSET_TEST_C_TOKEN_NUM 36
-
-	Token_C asset_test_tokens[ASSET_TEST_C_TOKEN_NUM] = {
-		{T_MACRO_INCLUDE, "#include"}, {T_WHITESPACE_SPACE, " "}, {T_MACRO_INCLUDE_LIBRARY_PATH, "stdio.h"}, {T_WHITESPACE_LINE_FEED, "\n"},			// LINE 1
-		{T_WHITESPACE_LINE_FEED, "\n"},																// LINE 2
-		{T_INT, "int"}, {T_WHITESPACE_SPACE, " "}, {T_IDENTIFIER, "main"}, {T_OPEN_PARENT, "("}, {T_CLOSING_PARENT, ")"}, {T_WHITESPACE_LINE_FEED, "\n"},	// LINE 3
-		{T_OPEN_BRACE, "{"}, {T_WHITESPACE_LINE_FEED, "\n"},													// LINE 4
-		{T_WHITESPACE_TAB, "\t"}, {T_COMMENT_LINE, "// This is a comment!"}, {T_WHITESPACE_LINE_FEED, "\n"},							// LINE 5
-		{T_WHITESPACE_TAB, "\t"}, {T_COMMENT_MULTILINE, "/*\n\t * This is a multi line\n\t * comment!\n\t */"}, {T_WHITESPACE_LINE_FEED, "\n"},			// LINE 6 - 9
-		{T_WHITESPACE_TAB, "\t"}, {T_IDENTIFIER, "printf"}, {T_OPEN_PARENT, "("}, {T_STRING, "Hello world!\\n"}, {T_CLOSING_PARENT, ")"},			// LINE 10
-		{T_SEMICOLON, ";"}, {T_WHITESPACE_LINE_FEED, "\n"}, 													// LINE 10
-		{T_WHITESPACE_LINE_FEED, "\n"},																// LINE 11
-		{T_WHITESPACE_TAB, "\t"}, {T_RETURN, "return"}, {T_WHITESPACE_SPACE, " "}, {T_NUMBER, "0"}, {T_SEMICOLON, ";"}, {T_WHITESPACE_LINE_FEED, "\n"},		// LINE 12
-		{T_CLOSING_BRACE, "}"}, {T_WHITESPACE_LINE_FEED, "\n"}, {T_EOF, "\0"},											// LINE 13
-	};
 
 	for (size_t i = 0; i < ASSET_TEST_C_TOKEN_NUM; i++) {
 		Token_C *token = lexer_c_next(lexer);
