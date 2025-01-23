dfa34fb35c84a331ada80e78db2a2889036182d9
JD297 <jan.dommasch297@gmail.com>
2025-01-23 19:22:06 +0100

    Implemented parser_c_parse_logical_or_expression

--- src/parser_c.c
+++ src/parser_c.c
@@ -799,22 +799,61 @@ ParseTreeNode_C *parser_c_parse_conditional_expression(Parser_C *parser)
 
 ParseTreeNode_C *parser_c_parse_logical_or_expression(Parser_C *parser)
 {
-    // TODO
-    (void)parser;
+    #define PTT_C_TYPE PTT_C_LOGICAL_OR_EXPRESSION
+    #define TOKEN_TYPE T_LOGICAL_OR
 
-    assert(0 && "Not implemented parser_c_parse_logical_or_expression");
+    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
 
-    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_LOGICAL_OR_EXPRESSION, NULL);
+    ParseTreeNode_C *logical_and_expression;
 
-    goto error;
+    const char *lexer_saved = parser->lexer->pbuf;
 
-    return this_node;
+    ParseTreeNode_C *left_node;
+    
+    Token_C *this_node_token;
+
+    parser_c_parse_required(parser, this_node, logical_and_expression, error);
+
+    if ((this_node_token = lexer_c_next_skip_whitespace(parser->lexer)) == NULL || this_node_token->type != TOKEN_TYPE) {
+        goto error;
+    }
+
+    parser_c_parse_required(parser, this_node, logical_and_expression, error);
+
+    this_node->token = this_node_token;
+
+    while (1) {
+        if ((this_node_token = lexer_c_next_skip_whitespace(parser->lexer)) == NULL || this_node_token->type != TOKEN_TYPE) {
+            goto ret;
+        }
+
+        this_node->token = this_node_token;
+
+        left_node = this_node;
+
+        this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
+
+        parse_tree_node_c_add(this_node, left_node);
+
+        parser_c_parse_required(parser, this_node, logical_and_expression, error);
+    }
+
+    ret: {
+        return this_node;
+    }
 
     error: {
+        parser->lexer->pbuf = lexer_saved;
+        
+        token_c_destroy(this_node_token);
+
         parse_tree_node_c_destroy(this_node);
 
         return NULL;
     }
+    
+    #undef PTT_C_TYPE
+    #undef TOKEN_TYPE
 }
 
 ParseTreeNode_C *parser_c_parse_expression(Parser_C *parser)
