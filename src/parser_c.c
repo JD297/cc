9fd238248711a40706fb101161a791cd267c5eba
JD297 <jan.dommasch297@gmail.com>
2025-01-23 20:02:36 +0100

    Implemented parser_c_parse_multiplicative_expression

--- src/parser_c.c
+++ src/parser_c.c
@@ -1374,22 +1374,69 @@ ParseTreeNode_C *parser_c_parse_additive_expression(Parser_C *parser)
 
 ParseTreeNode_C *parser_c_parse_multiplicative_expression(Parser_C *parser)
 {
-    // TODO
-    (void)parser;
+    #define PTT_C_TYPE PTT_C_MULTIPLICATIVE_EXPRESSION
+    #define TOKEN_TYPE_1 T_MULTIPLY
+    #define TOKEN_TYPE_2 T_DIVIDE
+    #define TOKEN_TYPE_3 T_MODULUS
 
-    assert(0 && "Not implemented parser_c_parse_multiplicative_expression");
+    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
 
-    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_MULTIPLICATIVE_EXPRESSION, NULL);
+    ParseTreeNode_C *cast_expression;
 
-    goto error;
+    const char *lexer_saved = parser->lexer->pbuf;
 
-    return this_node;
+    ParseTreeNode_C *left_node;
+    
+    Token_C *this_node_token;
+
+    parser_c_parse_required(parser, this_node, cast_expression, error);
+
+    if ((this_node_token = lexer_c_next_skip_whitespace(parser->lexer)) == NULL || (this_node_token->type != TOKEN_TYPE_1 && 
+                                                                                    this_node_token->type != TOKEN_TYPE_2 &&
+                                                                                    this_node_token->type != TOKEN_TYPE_3)) {
+        goto error;
+    }
+
+    parser_c_parse_required(parser, this_node, cast_expression, error);
+
+    this_node->token = this_node_token;
+
+    while (1) {
+        if ((this_node_token = lexer_c_next_skip_whitespace(parser->lexer)) == NULL || (this_node_token->type != TOKEN_TYPE_1 && 
+                                                                                        this_node_token->type != TOKEN_TYPE_2 &&
+                                                                                        this_node_token->type != TOKEN_TYPE_3)) {
+            goto ret;
+        }
+
+        this_node->token = this_node_token;
+
+        left_node = this_node;
+
+        this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
+
+        parse_tree_node_c_add(this_node, left_node);
+
+        parser_c_parse_required(parser, this_node, cast_expression, error);
+    }
+
+    ret: {
+        return this_node;
+    }
 
     error: {
+        parser->lexer->pbuf = lexer_saved;
+        
+        token_c_destroy(this_node_token);
+
         parse_tree_node_c_destroy(this_node);
 
         return NULL;
     }
+    
+    #undef PTT_C_TYPE
+    #undef TOKEN_TYPE_1
+    #undef TOKEN_TYPE_2
+    #undef TOKEN_TYPE_3
 }
 
 ParseTreeNode_C *parser_c_parse_cast_expression(Parser_C *parser)
