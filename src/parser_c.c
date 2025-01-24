eec19bdee2407608b40a5b285702bf91b3cd6c8c
JD297 <jan.dommasch297@gmail.com>
2025-01-24 19:36:38 +0100

    Implemented parser_c_parse_parameter_list

--- src/parser_c.c
+++ src/parser_c.c
@@ -1799,18 +1799,29 @@ ParseTreeNode_C *parser_c_parse_abstract_declarator(Parser_C *parser)
 
 ParseTreeNode_C *parser_c_parse_parameter_list(Parser_C *parser)
 {
-    // TODO
-    (void)parser;
-
-    assert(0 && "Not implemented parser_c_parse_parameter_list");
-
     ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_PARAMETER_LIST, NULL);
 
-    goto error;
+    ParseTreeNode_C *parameter_declaration;
+
+    const char* lexer_saved = parser->lexer->pbuf;
+
+    next_parameter_declaration_list: {
+        parser_c_parse_required(parser, this_node, parameter_declaration, error);
+
+        const char* lexer_saved_comma = parser->lexer->pbuf;
+
+        if (lexer_c_next_skip_whitespace_token_is_type(parser->lexer, T_COMMA) == 1) {
+            goto next_parameter_declaration_list;
+        }
+        
+        parser->lexer->pbuf = lexer_saved_comma;
+    }
 
     return this_node;
 
     error: {
+        parser->lexer->pbuf = lexer_saved;
+    
         parse_tree_node_c_destroy(this_node);
 
         return NULL;
