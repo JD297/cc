87cffda3d21a8d479a6e941784046711beb75c74
JD297 <jan.dommasch297@gmail.com>
2025-02-04 20:15:11 +0100

    Fixed parser_c_parse_logical_and_expression

--- src/parser_c.c
+++ src/parser_c.c
@@ -885,10 +885,7 @@ ParseTreeNode_C *parser_c_parse_expression(Lexer_C *lexer)
 
 ParseTreeNode_C *parser_c_parse_logical_and_expression(Lexer_C *lexer)
 {
-    #define PTT_C_TYPE PTT_C_LOGICAL_AND_EXPRESSION
-    #define TOKEN_TYPE T_LOGICAL_AND
-
-    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
+    ParseTreeNode_C *this_node = parse_tree_node_c_create(PTT_C_LOGICAL_AND_EXPRESSION, NULL);
 
     ParseTreeNode_C *inclusive_or_expression;
 
@@ -900,32 +897,44 @@ ParseTreeNode_C *parser_c_parse_logical_and_expression(Lexer_C *lexer)
 
     parser_c_parse_required(lexer, this_node, inclusive_or_expression, error);
 
-    if ((this_node_token = lexer_c_next_skip_whitespace(lexer)) == NULL || this_node_token->type != TOKEN_TYPE) {
+    if ((this_node_token = lexer_c_next_skip_whitespace(lexer)) == NULL) {
         goto error;
     }
 
-    parser_c_parse_required(lexer, this_node, inclusive_or_expression, error);
+    switch (this_node_token->type) {
+        case T_LOGICAL_AND: {
+            parser_c_parse_required(lexer, this_node, inclusive_or_expression, error);
 
-    this_node->token = this_node_token;
+            this_node->token = this_node_token;
+        } break;
+        default: goto ret;
+    }
 
     while (1) {
         const char *lexer_saved_token = lexer->pbuf;
 
-        if ((this_node_token = lexer_c_next_skip_whitespace(lexer)) == NULL || this_node_token->type != TOKEN_TYPE) {
-            lexer->pbuf = lexer_saved_token;
-
-            goto ret;
+        if ((this_node_token = lexer_c_next_skip_whitespace(lexer)) == NULL) {
+            goto error;
         }
 
-        this_node->token = this_node_token;
+        switch (this_node_token->type) {
+            case T_LOGICAL_AND: {
+                this_node->token = this_node_token;
 
-        left_node = this_node;
+                left_node = this_node;
 
-        this_node = parse_tree_node_c_create(PTT_C_TYPE, NULL);
+                this_node = parse_tree_node_c_create(PTT_C_LOGICAL_AND_EXPRESSION, NULL);
 
-        parse_tree_node_c_add(this_node, left_node);
+                parse_tree_node_c_add(this_node, left_node);
 
-        parser_c_parse_required(lexer, this_node, inclusive_or_expression, error);
+                parser_c_parse_required(lexer, this_node, inclusive_or_expression, error);
+            } break;
+            default: {
+                lexer->pbuf = lexer_saved_token;
+
+                goto ret;
+            }
+        }
     }
 
     ret: {
@@ -941,9 +950,6 @@ ParseTreeNode_C *parser_c_parse_logical_and_expression(Lexer_C *lexer)
 
         return NULL;
     }
-    
-    #undef PTT_C_TYPE
-    #undef TOKEN_TYPE
 }
 
 ParseTreeNode_C *parser_c_parse_inclusive_or_expression(Lexer_C *lexer)
