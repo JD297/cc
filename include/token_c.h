d50f1fbaf5277cd9e0e0e80642deb069a4fde8fc
JD297 <jan.dommasch297@gmail.com>
2025-06-22 08:24:22 +0200

    Removed token_c_create and token_c_destroy

--- include/token_c.h
+++ include/token_c.h
@@ -5,14 +5,10 @@
 #ifndef JD297_CC_TOKEN_C_H
 #define JD297_CC_TOKEN_C_H
 
-typedef struct Token_C {
+typedef struct {
     TokenType_C type;
     const char *value;
     size_t len;
 } Token_C;
-
-extern void *token_c_create(TokenType_C type, const char *value, size_t len);
-
-extern void token_c_destroy(Token_C *token);
 
 #endif
