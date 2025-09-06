fe5aca7a23d5d868b6f4a40576722fb6dc562d09
JD297 <jan.dommasch297@gmail.com>
2025-09-06 22:50:53 +0200

    Added ir functions [early WIP]

--- src/ir.c
+++ src/ir.c
@@ -1,13 +1,797 @@
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "ir.h"
 
 int ir_run(IR_CTX *ctx, ParseTreeNode_C *translation_unit)
 {
-	(void) ctx;
-	(void) translation_unit;
-
-	assert(0 && "TODO not implementend ir_run");
+	for (size_t i = 0; i < translation_unit->num; ++i) {
+		if (ir_external_declaration(ctx, translation_unit->elements[i]) != 0) {
+			return -1;
+		}
+	}
 
 	return 0;
 }
+
+int ir_external_declaration(IR_CTX *ctx, ParseTreeNode_C *external_declaration)
+{
+	for (size_t j = 0; j < external_declaration->num; ++j) {
+		ParseTreeNode_C *node = external_declaration->elements[j];
+
+		switch (node->type) {
+			case PTT_C_FUNCTION_DEFINITION: {
+				if (ir_function_definition(ctx, node) != 0) {
+					return -1;
+				}
+			} break;
+			case PTT_C_DECLARATION: {
+				if (ir_declaration(ctx, node) != 0) {
+					return -1;
+				}
+			} break;
+			default: {
+				assert(0 && "NOT REACHABLE");
+			}
+		}
+	}
+
+	return 0;
+}
+
+int ir_function_definition(IR_CTX *ctx, ParseTreeNode_C *function_definition)
+{
+	for (size_t i = 0; i < function_definition->num; ++i) {
+		ParseTreeNode_C *node = function_definition->elements[i];
+
+		switch (node->type) {
+			case PTT_C_DECLARATION_SPECIFIER: break;
+
+			case PTT_C_DECLARATOR: {
+				ParseTreeNode_C *direct_decl = node->elements[0];
+				
+				if (direct_decl->type != PTT_C_DIRECT_DECLARATOR) {
+					assert(0 && "Function Pointers are not supported!");
+				}
+				
+				ParseTreeNode_C *identifier_node = direct_decl->elements[0];
+				
+				if (identifier_node->type != PTT_C_IDENTIFIER) {
+					assert(0 && "Only simple functions with identifieres are supporteds!");
+				}
+
+				char *identifier = malloc(identifier_node->token.len + 1);
+				
+				assert(identifier != NULL);
+
+				strlcpy(identifier, identifier_node->token.value, identifier_node->token.len + 1);
+
+				SymtblEnt *entry = lmap_get(ctx->symtbl, identifier);
+
+				IRCode *code = malloc(sizeof(IRCode));
+				
+				assert(code != NULL);
+				
+				*code = (IRCode){
+					.op = IR_OC_FUNC_BEGIN,
+					.result = entry
+				};;
+
+				list_insert(ctx->code, list_end(ctx->code), code);
+
+				ctx->symtbl = (lmap_t *)entry->value;
+			} break;
+
+			// TODO IGNORE
+			case PTT_C_DECLARATION: break;
+
+			case PTT_C_COMPOUND_STATEMENT: {
+				char *endfuncstr = malloc(sizeof(char) * 32); // TODO HACK
+				
+				assert(endfuncstr != NULL);
+				
+				snprintf(endfuncstr, 31, ".Lfunc_end%zu", ctx->label_func_count);
+				
+				SymtblEnt *endent = malloc(sizeof(SymtblEnt));
+				*endent = (SymtblEnt){
+					.id = endfuncstr,
+					.use = LABEL
+				};
+				
+				lmap_add(ctx->symtbl, endfuncstr, endent);
+
+
+				if (ir_compound_statement(ctx, node) != 0) {
+					return -1;
+				}
+
+				IRCode *label = malloc(sizeof(IRCode));
+				
+				assert(label != NULL);
+
+				*label = (IRCode){
+					.op = IR_OC_LABEL,
+					.result = endent
+				};
+
+				list_insert(ctx->code, list_end(ctx->code), label);
+
+				IRCode *code = malloc(sizeof(IRCode));
+				
+				assert(code != NULL);
+				
+				*code = (IRCode){
+					.op = IR_OC_FUNC_END
+				};
+
+				list_insert(ctx->code, list_end(ctx->code), code);
+				
+				
+				ctx->symtbl = lmap_get(ctx->symtbl, "..");
+
+				++ctx->label_func_count;
+			} break;
+
+			default: assert(0 && "NOT REACHABLE");
+		}
+	}
+
+	return 0;
+}
+
+int ir_declaration(IR_CTX *ctx, ParseTreeNode_C *declaration)
+{
+	(void) ctx;
+	(void) declaration;
+
+	assert(0 && "TODO ir_declaration not implemented");
+}
+
+int ir_declaration_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_compound_statement(IR_CTX *ctx, ParseTreeNode_C *compound_statement)
+{
+	for (size_t i = 0; i < compound_statement->num; ++i) {
+		ParseTreeNode_C *node = compound_statement->elements[i];
+		
+		switch (node->type) {
+			case PTT_C_DECLARATION: {
+				if (ir_declaration(ctx, node) != 0) {
+					return -1;
+				}
+			} break;
+			case PTT_C_STATEMENT: {
+				if (ir_statement(ctx, node) != 0) {
+					return -1;
+				}
+			} break;
+			default: {
+				assert(0 && "NOT REACHABLE");
+			}
+		}
+	}
+
+	return 0;
+}
+
+int ir_storage_class_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_type_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_type_qualifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_struct_or_union_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_enum_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_typedef_name(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_struct_or_union(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_identifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_struct_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_specifier_qualifier(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_struct_declarator_list(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_struct_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_constant_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_pointer(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_direct_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_parameter_type_list(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_conditional_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_logical_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_logical_and_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_inclusive_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_exclusive_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_and_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_equality_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_relational_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_shift_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_additive_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_multiplicative_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_cast_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_unary_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_type_name(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_postfix_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_unary_operator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_primary_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_assignment_expression(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_constant(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_string(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_assignment_operator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_abstract_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_parameter_list(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_parameter_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_direct_abstract_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_enumerator_list(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_enumerator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_init_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_initializer(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_initializer_list(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_statement(IR_CTX *ctx, ParseTreeNode_C *statement)
+{
+	ParseTreeNode_C *node = statement->elements[0];
+	
+	switch (node->type) {
+		case PTT_C_LABELED_STATEMENT: {
+			if (ir_labeled_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		case PTT_C_EXPRESSION_STATEMENT: {
+			if (ir_jump_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		case PTT_C_COMPOUND_STATEMENT: {
+			// TODO change symtbl
+			if (ir_compound_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		case PTT_C_SELECTION_STATEMENT: {
+			if (ir_selection_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		case PTT_C_ITERATION_STATEMENT: {
+			if (ir_iteration_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		case PTT_C_JUMP_STATEMENT: {
+			if (ir_jump_statement(ctx, node) != 0) {
+				return -1;
+			}
+		} break;
+		default: {
+			assert(0 && "NOT REACHABLE");
+		}
+	}
+
+	return 0;
+}
+
+int ir_labeled_statement(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+	(void) ctx;
+	(void) this_node;
+
+	assert(0 && "TODO ir_labeled_statement not implemented");
+}
+
+int ir_expression_statement(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_selection_statement(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_iteration_statement(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+        (void) ctx;
+        (void) this_node;
+
+        assert(0 && "TODO not implemented");
+
+        return 0;
+}
+
+int ir_jump_statement(IR_CTX *ctx, ParseTreeNode_C *this_node)
+{
+		switch(this_node->token.type) {
+			case T_GOTO: {
+				assert(0 && "TODO not implemented (goto)");
+			} break;
+			case T_CONTINUE: {
+				assert(0 && "TODO not implemented (continue)");
+			} break;
+			case T_BREAK: {
+				assert(0 && "TODO not implemented (break)");
+			} break;
+			case T_RETURN: {
+				if (this_node->num > 0) {
+					//return 0;
+					//assert(0 && "TODO not implemented (return with expressions)");
+					fprintf(stderr, "TODO not implemented (return with expressions)\n");
+				}
+			
+				IRCode *code = malloc(sizeof(IRCode));
+				
+				assert(code != NULL);
+				
+				// TODO get symtblent Lfunc_end_%zu, label_func_count
+				char *endfuncstr = malloc(sizeof(char) * 32); // TODO HACK
+				
+				snprintf(endfuncstr, 31, ".Lfunc_end%zu", ctx->label_func_count);
+				
+				SymtblEnt *endent = lmap_get(ctx->symtbl, endfuncstr);
+
+				assert(endent != NULL);
+				
+				*code = (IRCode){
+					.op = IR_OC_JMP,
+					.result = endent
+				};
+
+				list_insert(ctx->code, list_end(ctx->code), code);
+				
+				return 0;
+			} break;
+			default: {
+				assert(0 && "NOT REACHABLE");
+			}
+		}
+
+        return 0;
+}
+
--- src/ir.h
+++ src/ir.h
@@ -16,7 +16,8 @@ typedef struct {
 typedef enum {
 	IR_OC_FUNC_BEGIN,
 	IR_OC_FUNC_END,
-	IR_OC_STORE,
+	IR_OC_LABEL,
+	IR_OC_JMP,
 	IR_OC_RET
 } IROpCode;
 
@@ -28,6 +29,74 @@ typedef struct {
 } IRCode;
 
 extern int ir_run(IR_CTX *ctx, ParseTreeNode_C *translation_unit);
+/*
+extern int ir_external_declaration(IR_CTX *ctx, ParseTreeNode_C *external_declaration);
+extern int ir_function_definition(IR_CTX *ctx, ParseTreeNode_C *function_definition);
+extern int ir_declaration(IR_CTX *ctx, ParseTreeNode_C *declaration);
+extern int ir_compound_statement(IR_CTX *ctx, ParseTreeNode_C *compound_statement);
+extern int ir_statement(IR_CTX *ctx, ParseTreeNode_C *statement);
+*/
+
+extern int ir_translation_unit(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_external_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_function_definition(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_declaration_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_compound_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_storage_class_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_type_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_type_qualifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_struct_or_union_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_enum_specifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_typedef_name(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_struct_or_union(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_identifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_struct_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_specifier_qualifier(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_struct_declarator_list(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_struct_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_constant_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_pointer(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_direct_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_parameter_type_list(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_conditional_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_logical_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_logical_and_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_inclusive_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_exclusive_or_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_and_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_equality_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_relational_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_shift_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_additive_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_multiplicative_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_cast_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_unary_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_type_name(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_postfix_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_unary_operator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_primary_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_assignment_expression(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_constant(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_string(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_assignment_operator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_abstract_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_parameter_list(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_parameter_declaration(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_direct_abstract_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_enumerator_list(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_enumerator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_init_declarator(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_initializer(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_initializer_list(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_labeled_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_expression_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_selection_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_iteration_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
+extern int ir_jump_statement(IR_CTX *ctx, ParseTreeNode_C *this_node);
 
 #endif
 
